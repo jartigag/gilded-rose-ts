4b66c27c6204752ef8168a58291a69b00bebd4b7
jartigag <jartigag@pm.me>
2021-12-11 12:24:49 +0100

    refactor: anidate ifs
    Co-authored-by: Leired7 <leired7@users.noreply.github.com>
    Co-authored-by: Rubén Jorajuría <rubenjl@users.noreply.github.com>

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -22,24 +22,18 @@ export class GildedRose {
     const backstageName = "Backstage passes to a TAFKAL80ETC concert";
     const sulfurasName = "Sulfuras, Hand of Ragnaros";
     if (item.name != agedBrieName && item.name != backstageName) {
-      if (item.quality > 0) {
-        if (item.name != sulfurasName) {
-          item.quality = item.quality - 1;
-        }
+      if (item.quality > 0 && item.name != sulfurasName) {
+        item.quality = item.quality - 1;
       }
     } else {
       if (item.quality < 50) {
         item.quality = item.quality + 1;
         if (item.name == backstageName) {
-          if (item.sellIn < 11) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
+          if (item.sellIn < 11 && item.quality < 50) {
+            item.quality = item.quality + 1;
           }
-          if (item.sellIn < 6) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
+          if (item.sellIn < 6 && item.quality < 50) {
+            item.quality = item.quality + 1;
           }
         }
       }
@@ -50,18 +44,14 @@ export class GildedRose {
     if (item.sellIn < 0) {
       if (item.name != agedBrieName) {
         if (item.name != backstageName) {
-          if (item.quality > 0) {
-            if (item.name != sulfurasName) {
-              item.quality = item.quality - 1;
-            }
+          if (item.quality > 0 && item.name != sulfurasName) {
+            item.quality = item.quality - 1;
           }
         } else {
           item.quality = item.quality - item.quality;
         }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-        }
+      } else if (item.quality < 50) {
+        item.quality = item.quality + 1;
       }
     }
     return item;
