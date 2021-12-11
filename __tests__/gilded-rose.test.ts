79c45ce00fb0f9a52e8dc88cd924e0a0a8955e0e
jartigag <jartigag@pm.me>
2021-12-11 10:00:51 +0100

    test: removes unnecesary Item test
    Co-authored-by: Leired7 <leired7@users.noreply.github.com>
    Co-authored-by: Rubén Jorajuría <rubenjl@users.noreply.github.com>

--- __tests__/gilded-rose.test.ts
+++ __tests__/gilded-rose.test.ts
@@ -1,9 +1,9 @@
 import { Item, GildedRose } from "../app/gilded-rose";
 
 describe("Gilded Rose", function () {
-  it("should foo", function () {
-    const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
+  it("Quality drops by one when a day passes", function () {
+    const gildedRose = new GildedRose([new Item("Elixir of the Mongoose", 5, 7)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("foo");
+    expect(items[0].quality).toEqual(6);
   });
 });
