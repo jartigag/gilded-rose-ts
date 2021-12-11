fe53c29c9ab17b1d840e655267185f12de835c94
jartigag <jartigag@pm.me>
2021-12-11 13:00:23 +0100

    refactor: guard function for sulfuras
    Co-authored-by: Leired7 <leired7@users.noreply.github.com>
    Co-authored-by: Rubén Jorajuría <rubenjl@users.noreply.github.com>

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -21,6 +21,11 @@ export class GildedRose {
     const agedBrieName = "Aged Brie";
     const backstageName = "Backstage passes to a TAFKAL80ETC concert";
     const sulfurasName = "Sulfuras, Hand of Ragnaros";
+
+    if (item.name == sulfurasName) {
+      return item;
+    }
+
     if (item.name == backstageName) {
       if (item.sellIn < 6) {
         item.quality = item.quality + 3;
@@ -31,18 +36,16 @@ export class GildedRose {
       }
     } else if (item.name == agedBrieName) {
       item.quality = item.quality + 1;
-    } else if (item.name != sulfurasName) {
+    } else {
       item.quality = item.quality - 1;
     }
-    if (item.name != sulfurasName) {
-      item.sellIn = item.sellIn - 1;
-    }
+
+    item.sellIn = item.sellIn - 1;
+
     if (item.sellIn < 0) {
       if (item.name != agedBrieName) {
         if (item.name != backstageName) {
-          if (item.name != sulfurasName) {
-            item.quality = item.quality - 1;
-          }
+          item.quality = item.quality - 1;
         } else {
           item.quality = item.quality - item.quality;
         }
@@ -50,12 +53,14 @@ export class GildedRose {
         item.quality = item.quality + 1;
       }
     }
+
     if (item.quality < 0) {
       item.quality = 0;
     }
     if (item.quality > 50 && item.name !== sulfurasName) {
       item.quality = 50;
     }
+
     return item;
   }
 
