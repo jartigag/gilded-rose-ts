251b38627d3f6da637be49071101e73287f19fcb
jartigag <jartigag@pm.me>
2021-12-11 12:22:31 +0100

    prettier
    Co-authored-by: Leired7 <leired7@users.noreply.github.com>
    Co-authored-by: Rubén Jorajuría <rubenjl@users.noreply.github.com>

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -1,73 +1,73 @@
 export class Item {
-    name: string;
-    sellIn: number;
-    quality: number;
+  name: string;
+  sellIn: number;
+  quality: number;
 
-    constructor(name, sellIn, quality) {
-        this.name = name;
-        this.sellIn = sellIn;
-        this.quality = quality;
-    }
+  constructor(name, sellIn, quality) {
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
 }
 
 export class GildedRose {
-    items: Array<Item>;
+  items: Array<Item>;
 
-    constructor(items = [] as Array<Item>) {
-        this.items = items;
-    }
+  constructor(items = [] as Array<Item>) {
+    this.items = items;
+  }
 
-    updateQualityForItem(item: Item): Item {
-        const agedBrieName = 'Aged Brie'
-        const backstageName = 'Backstage passes to a TAFKAL80ETC concert'
-        const sulfurasName = 'Sulfuras, Hand of Ragnaros'
-        if (item.name != agedBrieName && item.name != backstageName) {
-            if (item.quality > 0) {
-                if (item.name != sulfurasName) {
-                    item.quality = item.quality - 1
-                }
-            }
-        } else {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1
-                if (item.name == backstageName) {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1
-                        }
-                    }
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1
-                        }
-                    }
-                }
-            }
-        }
+  updateQualityForItem(item: Item): Item {
+    const agedBrieName = "Aged Brie";
+    const backstageName = "Backstage passes to a TAFKAL80ETC concert";
+    const sulfurasName = "Sulfuras, Hand of Ragnaros";
+    if (item.name != agedBrieName && item.name != backstageName) {
+      if (item.quality > 0) {
         if (item.name != sulfurasName) {
-            item.sellIn = item.sellIn - 1;
+          item.quality = item.quality - 1;
         }
-        if (item.sellIn < 0) {
-            if (item.name != agedBrieName) {
-                if (item.name != backstageName) {
-                    if (item.quality > 0) {
-                        if (item.name != sulfurasName) {
-                            item.quality = item.quality - 1
-                        }
-                    }
-                } else {
-                    item.quality = item.quality - item.quality
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1
-                }
+      }
+    } else {
+      if (item.quality < 50) {
+        item.quality = item.quality + 1;
+        if (item.name == backstageName) {
+          if (item.sellIn < 11) {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1;
             }
+          }
+          if (item.sellIn < 6) {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1;
+            }
+          }
         }
-        return item;
+      }
     }
+    if (item.name != sulfurasName) {
+      item.sellIn = item.sellIn - 1;
+    }
+    if (item.sellIn < 0) {
+      if (item.name != agedBrieName) {
+        if (item.name != backstageName) {
+          if (item.quality > 0) {
+            if (item.name != sulfurasName) {
+              item.quality = item.quality - 1;
+            }
+          }
+        } else {
+          item.quality = item.quality - item.quality;
+        }
+      } else {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
+        }
+      }
+    }
+    return item;
+  }
 
-    updateQuality() {
-        return this.items.map(this.updateQualityForItem);
-    }
+  updateQuality() {
+    return this.items.map(this.updateQualityForItem);
+  }
 }
